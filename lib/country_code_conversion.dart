8c5f6922b37b55874eccaa6be8cafad45041858a
CuongNV <cheapricetn@gmail.com>
2021-09-03 17:30:49 +0700

    Add CountryCodeConversion.

--- lib/country_code_conversion.dart
+++ lib/country_code_conversion.dart
@@ -1,7 +1,53 @@
 library country_code_conversion;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'languages.dart';
+
+class CountryCodeConversion {
+
+  /// get display name language by code.
+  Future<String?> getDisplayLanguageByCode(String languageCode) async {
+    if (languageCode.isEmpty) {
+      return null;
+    }
+
+    if (languages.containsKey(languageCode)) {
+      final String? displayLang = languages[languageCode]?.first;
+      return displayLang;
+    }
+
+    return null;
+  }
+
+  /// get display name English language by code.
+  Future<String?> getEnglishDisplayLanguageByCode(String languageCode) async {
+    if (languageCode.isEmpty) {
+      return null;
+    }
+
+    if (languages.containsKey(languageCode)) {
+      final String? displayLang = languages[languageCode]?.last;
+      return displayLang;
+    }
+
+    return null;
+  }
+
+  /// get language code by language name.
+  Future<String?> getLanguageCodeByName(String languageName) async {
+    if (languageName.isEmpty) {
+      return Future.value(null);
+    }
+
+    String? languageCode;
+    for (String key in languages.keys) {
+      for (String name in languages[key]!) {
+        if (languageName == name) {
+          languageCode = key;
+          return languageCode;
+        }
+      }
+    }
+
+    return languageCode;
+  }
 }
--- test/country_code_conversion_test.dart
+++ test/country_code_conversion_test.dart
@@ -1,12 +1,19 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:country_code_conversion/country_code_conversion.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+// import 'package:flutter_test/flutter_test.dart';
+//
+// import 'package:country_code_conversion/country_code_conversion.dart';
+//
+// void main() {
+//   test('adds one to input values', () async {
+//     final countryCodeConversion = CountryCodeConversion();
+//     String? name = await countryCodeConversion.getDisplayLanguageByCode("vi-VN");
+//     String? nameE = await countryCodeConversion.getEnglishDisplayLanguageByCode("vi-VN");
+//     String? code = await countryCodeConversion.getLanguageCodeByName("Tiếng Việt");
+//     String? codeE = await countryCodeConversion.getLanguageCodeByName("Vietnamese");
+//     print(name);
+//     print(nameE);
+//     print(code);
+//     print(codeE);
+//     String? codeA = await countryCodeConversion.getLanguageCodeByName("Arabic");
+//     print(codeA);
+//   });
+// }
