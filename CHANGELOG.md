59384fbf4b09cc08da3b9b0ab62c9d06ab8a438e
CuongNV <cuongnv3@sunshinegroup.vn>
2022-06-08 09:20:53 +0700

    remove flutter dependency.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
-## 1.0.1
-initial release.
-## 1.0.2
-Update README.md
+
+## 1.0.4
+remove flutter dependency.
 ## 1.0.3
 remove asynchronous processing.
+## 1.0.2
+Update README.md
+## 1.0.1
+initial release.
--- example/main.dart
+++ example/main.dart
@@ -1,43 +1,14 @@
 import 'package:country_code_conversion/country_code_conversion.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    final countryCodeConversion = CountryCodeConversion();
-    String? name = countryCodeConversion.getDisplayLanguageByCode("vi-VN");
-    String? nameE = countryCodeConversion.getEnglishDisplayLanguageByCode("vi-VN");
-    String? code = countryCodeConversion.getLanguageCodeByName("Tiếng Việt");
-    String? codeE = countryCodeConversion.getLanguageCodeByName("Vietnamese");
-    print(name);
-    print(nameE);
-    print(code);
-    print(codeE);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Example"),
-          ),
-          body: Center(child: Text("CountryCodeConversion")),
-        ),
-      ),
-    );
-  }
+  final countryCodeConversion = CountryCodeConversion();
+  String? name = countryCodeConversion.getDisplayLanguageByCode("vi-VN");
+  String? nameE =
+      countryCodeConversion.getEnglishDisplayLanguageByCode("vi-VN");
+  String? code = countryCodeConversion.getLanguageCodeByName("Tiếng Việt");
+  String? codeE = countryCodeConversion.getLanguageCodeByName("Vietnamese");
+  print(name);
+  print(nameE);
+  print(code);
+  print(codeE);
 }
